b3daaef89b58ec09d9d16ef268909ec863d3e915
aledurax <aledurax@gmail.com>
2018-11-02 14:55:54 +0000

    Frontend Folder

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+    styleUrls: ['./app.component.css'],
+  template: `<button class="btn btn-primary">Click Me</button>`,
+
+})
+export class AppComponent {
+  title = 'orcaestra-finanzas';
+}
