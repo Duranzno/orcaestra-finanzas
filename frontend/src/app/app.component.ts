a5041b1ac3dface63cd94e5644b666b4ad572eab
ale <aledurax@gmail.com>
2018-11-06 16:46:01 -0400

    Lets get on with it

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,15 +8,15 @@ import { PadreFormComponent} from './shared/padre-form/padre-form.component';
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.css'],
-  templateUrl:'./app.component.html'
+  templateUrl: './app.component.html'
 
 })
 export class AppComponent {
-  title = 'orcaestra-finanzas 3';
-  parentIsCollapsed=true;
-  studentIsCollapsed=true;
-  constructor(private modalService: NgbModal){}
-  modalStudent(){
+  title = 'orcaestra-finanzas';
+  parentIsCollapsed = true;
+  studentIsCollapsed = true;
+  constructor(private modalService: NgbModal) { }
+  modalStudent() {
     const modalRef = this.modalService.open(PagoFormComponent);
     // modalRef.componentInstance.name = 'World';
   }
