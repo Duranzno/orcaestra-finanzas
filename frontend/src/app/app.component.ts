3802dba8e194eb3eb5b9fbd3751817081b542d04
Duranzno <aledurax@gmail.com>
2018-11-06 11:09:20 -0400

    Gonna use rmate

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { PagoFormComponent} from './shared/pago-form/pago-form.component';
+import { EstudianteFormComponent} from './shared/estudiante-form/estudiante-form.component';
+import { PadreFormComponent} from './shared/padre-form/padre-form.component';
+
 
 @Component({
   selector: 'app-root',
@@ -8,4 +13,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orcaestra-finanzas 3';
+  parentIsCollapsed=true;
+  studentIsCollapsed=true;
+  constructor(private modalService: NgbModal){}
+  modalStudent(){
+    const modalRef = this.modalService.open(PagoFormComponent);
+    // modalRef.componentInstance.name = 'World';
+  }
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-// import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbCollapseModule,NgbModalModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { IconsModule } from './icons/icons.module'
 import { SharedModule }      from './shared/shared.module';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+
+import { EstudianteFormComponent} from './shared/estudiante-form/estudiante-form.component';
+import { PadreFormComponent} from './shared/padre-form/padre-form.component';
+import { PagoFormComponent} from './shared/pago-form/pago-form.component';
 
 
 @NgModule({
@@ -18,6 +21,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     FormsModule,
     SharedModule,
     //Theming
+    NgbCollapseModule,
+    NgbModalModule,
     // NgbModule,
   ],
   declarations: [
@@ -26,6 +31,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent,
   ],
   providers: [FormsModule],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents:[PagoFormComponent, EstudianteFormComponent, PadreFormComponent]
 })
 export class AppModule { }
