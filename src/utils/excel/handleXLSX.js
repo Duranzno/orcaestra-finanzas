9ff4d2b88e2088db09005003532dc4e6507904c5
alejandro <aledurax@gmail.com>
2019-03-14 23:08:22 -0400

    Ahora puede obtener valores desde diferentes sheets
    Signed-off-by: alejandro <aledurax@gmail.com>

--- src/utils/excel/handleXLSX.js
+++ src/utils/excel/handleXLSX.js
@@ -2,13 +2,19 @@ const XLSX = require('xlsx');
 const excelUtils = require('./extractXLSX');
 const excelUtilsM = require('./extractXLSXMarwan');
 
-module.exports.useSheetPaolaStyle = function(internalPath) {
+module.exports.useSheetPaolaStyle = function (internalPath) {
   let workbook = XLSX.readFile(internalPath);
-  let sheet = workbook.Sheets[workbook.SheetNames[0]];
-  excelUtils.extraerTodasFilas(sheet);
+  for (var i = 0; i < workbook.SheetNames.length; ++i) {
+    var sheet = workbook.Sheets[workbook.SheetNames[i]];
+    excelUtils.extraerTodasFilas(sheet);
+
+  }
 };
-module.exports.useSheetMarwanStyle = function(internalPath) {
+module.exports.useSheetMarwanStyle = function (internalPath) {
   let workbook = XLSX.readFile(internalPath);
-  let sheet = workbook.Sheets[workbook.SheetNames[0]];
-  excelUtilsM.extraerTodasFila(sheet);
+  for (var i = 0; i < workbook.SheetNames.length; ++i) {
+
+    var sheet = workbook.Sheets[workbook.SheetNames[i]];
+    excelUtilsM.extraerTodasFila(sheet);
+  }
 };
